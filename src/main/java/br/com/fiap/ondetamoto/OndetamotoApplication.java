4880b36f8cc2be3527b3f9da5b18c2a6285711ca
GuiRomanholi <guilhermeroma343@gmail.com>
2025-05-09 10:28:29 -0300

    Ajuste

--- src/main/java/br/com/fiap/ondetamoto/OndetamotoApplication.java
+++ src/main/java/br/com/fiap/ondetamoto/OndetamotoApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://localhost:8080/swagger-ui/index.html
 @SpringBootApplication
 @OpenAPIDefinition(info =
 @Info(title = "API OndeTaMoto", description = "API RESTful com Swagger para Projeto", version = "v1"))
