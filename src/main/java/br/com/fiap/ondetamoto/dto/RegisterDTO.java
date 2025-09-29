2b0ad4e59727a60abd105de7ff9c17a3d81a0601
GuiRomanholi <guilhermeroma343@gmail.com>
2025-09-29 19:45:18 -0300

    Att RegisterDto

--- src/main/java/br/com/fiap/ondetamoto/dto/RegisterDTO.java
+++ src/main/java/br/com/fiap/ondetamoto/dto/RegisterDTO.java
@@ -12,6 +12,6 @@ public record RegisterDTO(
         @NotBlank @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,25}$",
                 message = "A senha deve ter entre 8 e 25 caracteres, conter ao menos uma letra maiúscula e um número, " +
                         "e não pode ter caracteres especiais.") String senha,
-        @NotNull String role
+        @Pattern(regexp = "^(ADMIN|USER)$", message = "A role deve ser 'ADMIN' ou 'USER'") String role
 ) {
 }
