20f85f784dcbbb5f8313193e4d312ce35b7f676a
GuiRomanholi <guilhermeroma343@gmail.com>
2025-10-30 11:15:24 -0300

    Add Tailwind

--- src/main/java/br/com/fiap/ondetamoto/security/ApiSecurityConfigurations.java
+++ src/main/java/br/com/fiap/ondetamoto/security/ApiSecurityConfigurations.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 // ...
 @Configuration
-@EnableWebSecurity
 @Order(1)
 public class ApiSecurityConfigurations {
 
